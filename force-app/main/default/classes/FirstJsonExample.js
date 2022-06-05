5dc7b42335f37cc6e4f77c71b82da618ba85025d
Nisa Danaci <nisadanaci2@gmail.com>
2022-06-05 18:34:02 -0400

    JsonseriliazeCommit

---
+++ force-app/main/default/classes/FirstJsonExample.js
@@ -0,0 +1,24 @@
+// one account JSON below  hata vermesin diye kapattim
+// {
+//     "FirstName": "Plain",
+//     "lastName": " Text",
+//     "AnnualRevenue": 50000.99,
+//     "VIP__c": true
+// }
+
+//list of account in JSON
+[
+    {
+        "FirstName": "Plain",
+        "lastName": " Text",
+        "AnnualRevenue": 50000.99,
+        "VIP__c": true
+    },
+    {
+        "FirstName": "Plain2",
+        "lastName": " Text2",
+        "AnnualRevenue": 50002.99,
+        "VIP__c": true
+    }
+
+]
