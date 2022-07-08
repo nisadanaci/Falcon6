9f89f9a968aceef43e46391057aca71282485942
Nisa Danaci <nisadanaci2@gmail.com>
2022-07-08 10:36:06 -0400

    LWC Track and Two way

---
+++ force-app/main/default/lwc/trackProperty/trackProperty.js
@@ -0,0 +1,25 @@
+import { LightningElement, track } from 'lwc';
+
+export default class TrackProperty extends LightningElement {
+   @track location = {
+        city: "Indiana",
+        country: "United States",
+        postalCode: "46074"
+
+    };
+    handleChange(event){
+        this.location.city = event.target.value;
+    }
+}
+// //import {lightning element, track } from 'lwc';
+//export default class TRACKPROPERTY extends LightningElement{
+
+// @track
+//location = {@track is thats a decorator stating it is a reactive }
+
+
+/*details : {
+   name : "Jeff Bezos",
+   title : "Executive Chairman",
+   company : "Amazon"
+}*/
