a0f1b52e7d9b0060fb3166864490bdb9f80cefeb
Nisa Danaci <nisadanaci2@gmail.com>
2022-07-08 23:40:46 -0400

    templateLooping1

---
+++ force-app/main/default/lwc/templateLooping1/templateLooping1.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class TemplateLooping1 extends LightningElement {
+    fruits = ["Apple", "Mango", "Strawberry","Watermelon"];
+
+  
+}
+
+
+// /*
+// contacts = [
+//         {firstname: "Jeff", lastname: "Bezos", title: "Executive Chairman"},
+//         {firstname: "Satya", lastname: "Nadella", title: "Chief Executive Officer"},
+//         {firstname: "Sundar", lastname: "Pichai", title: "Chief Executive Officer"}
+// ];/*
