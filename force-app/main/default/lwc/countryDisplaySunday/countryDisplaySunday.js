efa77da4137d804f7e13468340bf5e5964372f46
Nisa Danaci <nisadanaci2@gmail.com>
2022-07-17 10:52:53 -0400

    country Display Sunday 2

---
+++ force-app/main/default/lwc/countryDisplaySunday/countryDisplaySunday.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class CountryDisplaySunday extends LightningElement {
+    countries = [
+        "United States",
+        "United Arab Emirates",
+        "Singapore",
+        "Turkey",
+        "Malaysia",
+        "Japan"
+    ];
+}
